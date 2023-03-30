114de8843a3b50b6d3b5db11422273fd9730a216
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-03-30 16:53:08 +0200

    add ex2,3,4 and report

--- app.py
+++ app.py
@@ -29,6 +29,50 @@ def issue_opened_event(repo, payload):
     issue.create_comment(f"{response}")
     issue.add_to_labels("Bug")
 
+def pull_request_merged_event(repo, payload):
+    pull = repo.get_pull(number=payload['pull_request']['number'])
+    author = pull.user.login
+
+    response = f"Thanks @{author}, the pull request has been merged!"
+
+    pull.create_issue_comment(f"{response}")
+    repo.get_git_ref(f"heads/{payload['pull_request']['head']['ref']}").delete()
+
+def pull_request_pending_event(repo, payload, old_title = None):
+    pull = repo.get_pull(number=payload['pull_request']['number'])
+    title = payload['pull_request']['title']
+
+    name = ['WIP', "work in progress", "do not merge"]
+    was_wip = old_title and any(n in old_title.lower() for n in name)
+
+    if any(n in title for n in name):
+        # set the status to pending
+        pull.get_commits().reversed[0].create_status(
+            state='pending',
+            description='Work in progress',
+            context='WIP'
+        )
+    elif was_wip:
+        pull.get_commits().reversed[0].create_status(
+            state='success',
+            description='Ready for review',
+            context='WIP'
+        )
+
+
+def pull_request_edited(repo, payload):
+    old_title = None
+    if 'title' in payload['changes']:
+        old_title = payload['changes']['title']['from']
+    pull_request_pending_event(repo, payload, old_title=old_title)
+    
+
+
+def pull_request_opened(repo, payload):
+    pull_request_pending_event(repo, payload)
+
+
+
 @app.route("/", methods=['POST'])
 def bot():
     payload = request.json
@@ -49,6 +93,16 @@ def bot():
     # Check if the event is a GitHub issue creation event
     if all(k in payload.keys() for k in ['action', 'issue']) and payload['action'] == 'opened':
         issue_opened_event(repo, payload)
+
+    # Check if the event is a GitHub merged pull request event
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed' and payload['pull_request']['merged']:
+        pull_request_merged_event(repo, payload)
+
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
+        pull_request_opened(repo, payload)
+
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'edited':
+        pull_request_edited(repo, payload)
 
     return "", 204
 
